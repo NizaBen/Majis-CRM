3796b73059c57fdfd133bda730358a39ad914b27
NizaBen <benamornizar17@gmail.com>
2025-12-26 20:29:15 +0100

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function App() {
+    return (
+      <div className="bg-gray-100 min-h-screen font-sans">
+        <h2>Welcome to Majis CRM</h2>
+        <div className="bg-red-500 text-white p-4">
+          Tailwind fonctionne
+        </div>
+      </div>
+    );
+};
+
+export default App;
